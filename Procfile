52ef67699fd6d8e9273836f07bac9971f5be698a
Blandaren123 <toskha_91@hotmail.se>
2022-05-07 02:05:32 +0000

    deployment part 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn toysstore.wsgi:application
--- toysstore/settings.py
+++ toysstore/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-c2q*idprtw59bht^+kvs0wdy@2qte*p+7eky#&=8_n7_=cqzmc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['toysstore-udd.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -118,12 +118,18 @@ WSGI_APPLICATION = 'toysstore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 
 
