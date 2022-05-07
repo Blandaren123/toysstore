e984e6db68a0d40da59c5158764b0c8de2ef4ec9
Blandaren123 <toskha_91@hotmail.se>
2022-05-07 02:24:21 +0000

    removed secret ket and set debug

--- toysstore/settings.py
+++ toysstore/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c2q*idprtw59bht^+kvs0wdy@2qte*p+7eky#&=8_n7_=cqzmc'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['toysstore-udd.herokuapp.com', 'localhost']
 
