0cefdf8b45ea6e3bebb09453e4026369937166f9
Blandaren123 <toskha_91@hotmail.se>
2022-06-12 07:26:29 +0000

    made about page

--- home/views.py
+++ home/views.py
@@ -5,3 +5,7 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+def about(request):
+    """ A view to return the about page """
+    return render(request, 'home/about.html')
