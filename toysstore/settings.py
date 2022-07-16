faa749746c2003375dacd49133265a2d285d5da0
Blandaren123 <toskha_91@hotmail.se>
2022-07-16 04:24:33 +0000

    set debug to true

--- toysstore/settings.py
+++ toysstore/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = 'FALSE' in os.environ
 
 ALLOWED_HOSTS = ['toysstore-udd.herokuapp.com', 'localhost']
 
